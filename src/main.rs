3ad6a5c426a5a846d2710350dd709175adda1499
lukasjoc <jochamlu@gmail.com>
2021-07-06 00:02:42 +0200

    add structs and figure out a way to work with the file tree

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
 use std::path::PathBuf;
-use str::parse;
+use watch::Files;
 mod cmd;
 mod watch;
 
 // TODO: watch for expose.toml config file
 // TODO: take default one as default config
 // Eval if we need this even !
-//
 fn main() {
     let matches = cmd::build_cmd().get_matches();
 
@@ -18,16 +17,24 @@ fn main() {
             .unwrap_or_else(|_| panic!("Cannot start watching file: {}", other)),
     };
 
-    let interval = matches
-        .value_of("interval")
-        .unwrap()
-        .parse::<u64>()
-        .unwrap();
-
-    let port = matches.value_of("port").unwrap().parse::<u64>().unwrap();
-
-    let host = matches.value_of("host").unwrap();
+    //     let interval = matches
+    //         .value_of("interval")
+    //         .unwrap()
+    //         .parse::<u64>()
+    //         .unwrap();
+    //
+    //     let port = matches.value_of("port").unwrap().parse::<u64>().unwrap();
+    //     let host = matches.value_of("host").unwrap();
 
     // TODO: Signals Handling, Watching
-    watch::changes(path.display().to_string(), interval, port, host);
+    let path: &str = path.to_str().unwrap();
+    let path_tree: Files = Files::new(path);
+
+    println!("{:?}", path_tree);
+
+    watch::changes(path, 0, path_tree);
+    //     watch::changes(path, interval, path_tree);
+
+    // build the initial path before starting wathching
+    // watch::changes(path.display().to_string(), interval, port, host);
 }
--- src/watch.rs
+++ src/watch.rs
@@ -1,56 +1,78 @@
 extern crate notify;
-use notify::{watcher, RecursiveMode, Watcher};
+use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
+use std::collections::HashMap;
+use std::fs::*;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
-use std::time::Duration;
 use std::time::SystemTime;
 
 // This is a struct with info about
 // a file in the tree
 // contains info about file, type, size, content ...
 #[derive(Debug)]
-pub struct FileInfo {
-    path: PathBuf,
-    ftype: String,
-    fsize: u64,
-    fmod: u16,
+struct FileMeta {
     modified: SystemTime,
     accessed: SystemTime,
     created: SystemTime,
 }
 
+#[derive(Debug)]
+struct FileInfo {
+    path: PathBuf,
+    file_type: &'static str,
+    file_size: u64,
+    file_mod: u16,
+    file_meta: FileMeta,
+}
+
 // This is the files tree
 #[derive(Debug)]
 pub struct Files {
-    files: Vec<FileInfo>,
-    // meta
-    // TODO: accumulated size
-    // number of files
-    // number of dirs
-    // ...
+    files: HashMap<&'static str, FileInfo>,
+    // TODO: meta data about the hashmap
 }
 
-pub fn changes(path: PathBuf, interval: u64, port: u64, hostname: &str) {
+impl Files {
+    pub fn new(path: &str) -> Self {
+        let files: HashMap<&'static str, FileInfo> = HashMap::new();
+        for entry in read_dir(path) {
+            println!("E: {:?}", entry);
+        }
+        Files { files }
+    }
+    // return if the key is contained in the struct files
+    // member
+    pub fn contains_key(&self, k: &str) -> bool {
+        self.files.contains_key(&k)
+    }
+}
+
+// given a path watches it by an interval
+pub fn changes(path: &str, interval: u64, map: Files) {
     let (tx, rx) = channel();
-
-    let mut watcher = watcher(tx, Duration::from_secs(interval)).unwrap();
-
-    let watch_path: String = path.display().to_string();
-
-    watcher.watch(watch_path, RecursiveMode::Recursive).unwrap();
+    let mut watcher = raw_watcher(tx).unwrap();
+    watcher.watch(path, RecursiveMode::Recursive).unwrap();
 
     // keep watching the rec
     loop {
         match rx.recv() {
-            //Ok(RawEvent {
-            //    path: Some(path),
-            //    op: Ok(op),
-            //    cookie,
-            //}) => {
-            //    println!("=> RAW: {:?} {:?} ({:?})", op, path, cookie);
-            //}
-            // Ok(event) => println!("=> BROKEN: broken event: {:?}", event),
-            //Err(e) => println!("=> ERR: watch error: {:?}", e),
+            Ok(RawEvent {
+                path: Some(path),
+                op: Ok(op),
+                cookie,
+            }) => {
+                if map.contains_key(path.to_str().unwrap()) {
+                    // update element
+                    println!("Action Received: {:?} {:?} ({:?})", op, path, cookie);
+                    println!("path found in map: {:?}", path);
+                } else {
+                    // add new element (on create actions)
+                    println!("Action Received: {:?} {:?} ({:?})", op, path, cookie);
+                    println!("path not in map: {:?}", path);
+                }
+            }
+            Ok(event) => println!("broken event: {:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
         }
     }
 }
